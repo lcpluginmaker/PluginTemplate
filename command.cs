cc1259c50a6a7858c585294e1d3339286714ffd7
alexcoder04 <alexcoder04@protonmail.com>
2022-07-15 15:57:09 +0200

    adapt for new API (v2.1.0)

--- command.cs
+++ command.cs
@@ -12,10 +12,10 @@ namespace LeoConsole_PluginTemplate
     // TODO: change these to your command name and description
     public string Name { get { return "plugin-template-test"; } } // this what you type into the console to run the command
     public string Description { get { return "test plugin template"; } } // this is the description in the help command
-    public Action CommandFunktion { get { return () => Command(); } }
-    public Action HelpFunktion { get { return () => Help(); } }
-    private string[] _InputProperties;
-    public string[] InputProperties { get { return _InputProperties; } set { _InputProperties = value; } }
+    public Action CommandFunction { get { return () => Command(); } }
+    public Action HelpFunction { get { return () => Help(); } }
+    private string[] _Arguments;
+    public string[] _Arguments { get { return _Arguments; } set { _Arguments = value; } }
     public IData data = new ConsoleData();
 
     // --- this is what actually runs ---
@@ -24,9 +24,10 @@ namespace LeoConsole_PluginTemplate
       LConsole.WriteLine("§aHello world!§r"); // colored hello world
     }
 
+    // --- your help message ---
     public void Help()
     {
-      LConsole.WriteLine("§aHello world!§r");
+      LConsole.WriteLine("§aThis plugin prints 'Hello World!'§r");
     }
   }
 }
